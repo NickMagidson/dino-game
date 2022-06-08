85d9413dd3ac0754ceafe6dcaf7708dd9a94f945
Nick Magidson <Nickmagi24@gmail.com>
2022-06-08 10:02:14 -0400

    World scaling and responsivness

--- script.js
+++ script.js
@@ -7,5 +7,13 @@ setPixelToWorldScale();
 window.addEventListener('resize', setPixelToWorldScale);
 
 function setPixelToWorldScale() {
-    
+    let worldToPixelScale;
+    if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
+        worldToPixelScale = window.innerWidth / WORLD_WIDTH
+    } else {
+        worldToPixelScale = window.innerHeight / WORLD_HEIGHT
+    }
+
+    worldElem.style.width = `${WORLD_WIDTH * worldToPixelScale}px`
+    worldElem.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`
 }
